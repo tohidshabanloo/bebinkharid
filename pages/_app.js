cafad0a725fa621c7c6d4de58b1fe2276d51cd5b
tohidshabanloo <tshabanloo@hotmail.com>
2022-08-27 20:52:47 +0430

    install font and make site rtl

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../styles/font.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div dir="rtl">
+      <main className="container">
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
