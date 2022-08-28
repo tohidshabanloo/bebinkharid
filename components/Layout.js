750c32d607c09c8bda04d8059f6d5d677a9c80c7
tohidshabanloo <tshabanloo@hotmail.com>
2022-08-28 21:40:10 +0430

    added logo

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
 import Link from "next/link";
 import React from "react";
@@ -13,9 +14,13 @@ const Layout = ({ children, title }) => {
       <div className="flex min-h-screen flex-col justify-between ">
         <header>
           <nav className="flex h-12 items-center px-4 justify-between shadow-md">
-            <Link href="/">
-              <a className="text-lg font-bold">ببین خرید</a>
-            </Link>
+            <div className="flex justify-between gap-4">
+              <img width={25} height={20} src="/images/logo.png" alt="" />
+              <Link href="/">
+                <a className="text-lg font-bold flex">ببین خرید</a>
+              </Link>
+            </div>
+
             <div>
               <Link href="/cart">
                 <a className="p-2">کارت</a>
